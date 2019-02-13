923127cf9eca081ef74d31a5691412e31baa4541
sdttttt <760159537@qq.com>
2019-02-13 21:12:27 +0800

    第一次

---
+++ src/main/java/com/moma/MomaApplication.java
@@ -0,0 +1,14 @@
+package com.moma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomaApplication.class, args);
+    }
+
+}
+
