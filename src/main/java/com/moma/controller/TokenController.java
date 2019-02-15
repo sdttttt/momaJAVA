79a5030e7b43be6b3fbe6b9e27683a08fdac08a4
sdttttt <760159537@qq.com>
2019-02-16 02:48:55 +0800

    减小和微信接口之间的耦合度

--- src/main/java/com/moma/controller/TokenController.java
+++ src/main/java/com/moma/controller/TokenController.java
@@ -2,9 +2,11 @@ package com.moma.controller;
 
 import com.moma.exception.VerifyFoundException;
 import com.moma.service.TokenService;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -14,8 +16,8 @@ public class TokenController {
     TokenService tokenService;
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public Map<String,Object> getToken(@RequestParam(value = "code") String code){
-
+    public Map<String,Object> getToken(@RequestBody Map<String,Object> map){
+        String code = (String) map.get("code");
         return tokenService.getToken(code);
     }
 
